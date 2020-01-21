76878dadf4d8feba1948dd42e9539589916055ba
Aju Tamang <ajutamang90@gmail.com>
2020-01-21 22:30:25 +0545

    updated background scene

--- game/main.c
+++ game/main.c
@@ -24,8 +24,15 @@ return 0;
 void print_line1(){
 setcolor(WHITE);
 line(0,18,640,18);
+line(0,19,640,19);
+line(0,20,640,20);
+line(0,21,640,22);
 }
 
 void print_line2(){
 line(0,465,640,465);
+line(0,466,640,466);
+line(0,467,640,467);
+line(0,468,640,468);
+
 }
