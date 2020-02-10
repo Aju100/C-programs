33737d202456031e4c143e99ffe84fe5eef1a71b
Aju Tamang <ajutamang90@gmail.com>
2020-02-10 19:51:40 +0545

    Added other file

---
+++ others/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int add();
+
+int main(int argc, const char *argv[]){
+    int x,y;
+    add(x,y);
+    return 0;
+}
+
+int add(int param1,param2) {
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int sum = 0;
+    sum = a+b;
+    printf("%d",sum);
+}
